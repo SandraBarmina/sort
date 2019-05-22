d155951c8ec200dbb45c56dbfd40c3e41a3e9b05
Unknown <SandraSands38@gmail.com>
2019-05-22 10:18:52 +0700

    fixed working with threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,13 +19,19 @@ import java.util.stream.Collectors;
 public class Main {
 
     enum ContentType {i, s}
+
     enum SortMode {a, d}
 
-    @Parameters(paramLabel = "FILE", arity = "1..", index = "0", description = "The file with data to sort.") File inputFile;
-    @Option(names = {"-o", "--out-prefix"}, description = "Output file prefix.", required = true) String prefix;
-    @Option(names = {"-c", "--content-type"}, description = "Content type (String or Integer). Valid values: ${COMPLETION-CANDIDATES}", required = true) ContentType type;
-    @Option(names = {"-s", "--sort-mode"}, description = "Sorting type (ascending or descending). Valid values: ${COMPLETION-CANDIDATES}", required = true) SortMode sortMode;
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message") boolean helpRequested = false;
+    @Parameters(paramLabel = "FILE", arity = "1..", index = "0", description = "The file with data to sort.")
+    File inputFile;
+    @Option(names = {"-o", "--out-prefix"}, description = "Output file prefix.", required = true)
+    String prefix;
+    @Option(names = {"-c", "--content-type"}, description = "Content type (String or Integer). Valid values: ${COMPLETION-CANDIDATES}", required = true)
+    ContentType type;
+    @Option(names = {"-s", "--sort-mode"}, description = "Sorting type (ascending or descending). Valid values: ${COMPLETION-CANDIDATES}", required = true)
+    SortMode sortMode;
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display a help message")
+    boolean helpRequested = false;
 
     public static void main(String[] args) {
         Main app = CommandLine.populateCommand(new Main(), args);
@@ -37,38 +43,30 @@ public class Main {
 
         SortingType sortingType = app.sortMode.equals(SortMode.a) ? SortingType.ASCENDING : SortingType.DESCENDING;
 
-        Runnable task = () -> {
-            String absolutePath = app.inputFile.getAbsolutePath();
-            File[] files = Optional.ofNullable(app.inputFile.listFiles())
-                            .orElseThrow(() -> new FileProcessingError("Incorrect directory: " + absolutePath));
+        String absolutePath = app.inputFile.getAbsolutePath();
+        File[] files = Optional.ofNullable(app.inputFile.listFiles())
+                .orElseThrow(() -> new FileProcessingError("Incorrect directory: " + absolutePath));
 
-            Arrays.stream(files).filter(File::isFile).forEach(file -> {
+        Arrays.stream(files).filter(File::isFile).parallel().forEach((File file) -> {
+            String absoluteOutputFilePath = file.getParentFile().getAbsolutePath()
+                    + File.separator + app.prefix + file.getName();
 
-                String absoluteOutputFilePath = file.getParentFile().getAbsolutePath()
-                                        + File.separator + app.prefix + file.getName();
-                try {
-                    Path path = Paths.get(absoluteOutputFilePath);
-                    Reader reader = new Reader();
+            try {
+                Path path = Paths.get(absoluteOutputFilePath);
+                Reader reader = new Reader();
 
-                    if (app.type.equals(ContentType.s)) {
-                        ArrayList<String> arr = reader.readStringFile(file);
-                        new SortingImpl<String>().sort(arr, sortingType);
-                        Files.write(path, arr);
-                    } else {
-                        ArrayList<Integer> arrInt = reader.readIntFile(file);
-                        new SortingImpl<Integer>().sort(arrInt, sortingType);
-                        Files.write(path, arrInt.stream().map(String::valueOf).collect(Collectors.toList()));
-                    }
-                } catch (IOException e) {
-                    throw new FileProcessingError("Error writing file: " + absoluteOutputFilePath, e);
+                if (app.type.equals(ContentType.s)) {
+                    ArrayList<String> arr = reader.readStringFile(file);
+                    new SortingImpl<String>().sort(arr, sortingType);
+                    Files.write(path, arr);
+                } else {
+                    ArrayList<Integer> arrInt = reader.readIntFile(file);
+                    new SortingImpl<Integer>().sort(arrInt, sortingType);
+                    Files.write(path, arrInt.stream().map(String::valueOf).collect(Collectors.toList()));
                 }
-            });
-        };
-
-        Thread thread = new Thread(task);
-        thread.start();
-
-        Thread thread2 = new Thread(task);
-        thread2.start();
+            } catch (IOException e) {
+                throw new FileProcessingError("Error writing file: " + absoluteOutputFilePath, e);
+            }
+        });
     }
 }
